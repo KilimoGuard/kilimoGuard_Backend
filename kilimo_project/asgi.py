5e78e608a394a5c12ea6a6f3b09a43b6b43b4d77
petermirithu <peter_mirithu@yahoo.com>
2024-08-06 03:08:00 +0300

    rename project folder

--- kilimo_project/asgi.py
+++ kilimo_project/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for project_settings project.
+ASGI config for kilimo_project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project_settings.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kilimo_project.settings")
 
 application = get_asgi_application()
--- kilimo_project/wsgi.py
+++ kilimo_project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for project_settings project.
+WSGI config for kilimo_project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project_settings.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kilimo_project.settings")
 
 application = get_wsgi_application()
