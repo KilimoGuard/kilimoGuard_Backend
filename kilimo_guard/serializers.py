47f3cdb8b391a9f156ee07719699cbdd793d436a
Michael Khanda <michaelkhanda006@gmail.com>
2024-08-08 13:14:27 +0300

    added authentication functionality
    Co-authored-by: Pyra <pyra_m.k@yahoo.com>

--- kilimo_guard/serializers.py
+++ kilimo_guard/serializers.py
@@ -1,8 +1,15 @@
 from rest_framework_mongoengine import serializers
 from kilimo_guard.models import *
 
+
 class UsersSerializer(serializers.DocumentSerializer):
-    model = Users
+
     class Meta:
-        model = Users  
-        fields="__all__"
+        model = Users
+        # fields = "__all__"
+        exclude = ("password",)
+        extra_kwargs = {'password': {'write_only': True, 'min_length': 8}}
+
+    def create(self, validated_data):
+        # Remove make_password and let the model handle password hashing
+        return super(UsersSerializer, self).create(validated_data)
